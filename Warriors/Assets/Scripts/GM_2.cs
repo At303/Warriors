0854e67b92bf988cd5fa0a479ef763d00fd93a58
At303 <clrch4n@gmail.com>
2016-02-09 23:44:10 +0900

    [GM] 2nd scene 배경 작업.

--- Warriors/Assets/Scripts/GM_2.cs
+++ Warriors/Assets/Scripts/GM_2.cs
@@ -3,12 +3,9 @@ using System.Collections;
 using gamedata;
 
 public class GM_2 : MonoBehaviour {
-	GameObject label;
 
 	// Use this for initialization
 	void Start () {
-		label = GameObject.Find ("s2_test");
-		label.GetComponent<UILabel> ().text = GameData.chest_struct.Level.ToString ();
 	}
 	
 	// Update is called once per frame
