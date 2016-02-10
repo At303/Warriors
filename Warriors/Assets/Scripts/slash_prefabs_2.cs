4537df7b550c3ad680eb89bbcb69a61788a7fc78
At303 <clrch4n@gmail.com>
2016-02-10 22:55:24 +0900

    [GM] 보스 씬 구현

--- Warriors/Assets/Scripts/slash_prefabs_2.cs
+++ Warriors/Assets/Scripts/slash_prefabs_2.cs
@@ -7,23 +7,29 @@ public class slash_prefabs_2 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		move_prefabs = new GameObject[5];
+		move_prefabs = new GameObject[10];
 		prefabx_make();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 		move_prefabs_func (move_prefabs [0], 0);
 		move_prefabs_func (move_prefabs [1], 1);
 		move_prefabs_func (move_prefabs [2], 2);
 		move_prefabs_func (move_prefabs [3], 3);
 		move_prefabs_func (move_prefabs [4], 4);
-		move_prefabs_func (move_prefabs [1], 5);
+		move_prefabs_func (move_prefabs [5], 5);
+		move_prefabs_func (move_prefabs [6], 6);
 
 		if (Input.GetMouseButtonDown (0)) 
 		{
-			      
+			print ("name : " + move_prefabs [0].name);
+			print ("name : " + move_prefabs [1].name);
+			print ("name : " + move_prefabs [2].name);
+
+
+
+
 		}
 
 	}
@@ -33,13 +39,16 @@ public class slash_prefabs_2 : MonoBehaviour {
 	}
 	void prefabx_make()
 	{
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 9; i++) {
 			// prefabs 생성.
-			NGUITools.AddChild (this.gameObject, prefabs [i]);
+			int index = Random.Range(0,5);
+			NGUITools.AddChild (this.gameObject, prefabs[index]);
 
 			// prefabs name으로 gambeobjct가져오기
-			string prefabs_name = "slash0" + (i + 1).ToString () + "_prefabs(Clone)";
+			string prefabs_name = "slash_prefabs" + (index).ToString () + "(Clone)";
+
 			move_prefabs [i] = GameObject.Find (prefabs_name);
+			move_prefabs [i].name = "prefabs"+i.ToString();
 		}
 	}
 
